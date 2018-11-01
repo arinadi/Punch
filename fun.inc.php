4234728a4b77b526b2f76768e40b4cc7bcb4c453
Arinadi <arinadi@maju-bersama.com>
2018-11-01 09:12:46 +0700

    V2 Support MySQLi (PHP 5.6+)

--- fun.inc.php
+++ fun.inc.php
@@ -1,39 +1,37 @@
 <?php
+ini_set('max_execution_time', 3000);
 include("config.php");
-function db_connect(){	
-	$connection_server=mysql_connect(DB_HOST,DB_USERNAME,DB_PASSWORD);
+function db_connect(){
+	$connection_server=mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_DB);
 	if(!$connection_server){
 	   return false;	
 	}
-	$connection_db=mysql_select_db(DB_DB);
-	if(!$connection_db){
-		return false;
-	}	
-		return $connection_server;
+	
+	return $connection_server;
 }	
 
 function escape_data($data) { 
-	db_connect();
+	$dbc = db_connect();
 	$data = stripslashes($data);	
-	return mysql_real_escape_string(trim($data));
+	return mysqli_real_escape_string($dbc, trim($data));
 }
 
 function get_tables(){
-	db_connect();
-	$result = mysql_query("SHOW tables FROM ".$dbname);
+	$dbc = db_connect();
+	$result = mysqli_query($dbc,"SHOW tables FROM ".$dbname);
 		$tables = array();
 		$q = 0;
 		$v = 0;
-		while ($row = mysql_fetch_array($result)) {
+		while ($row = mysqli_fetch_array($result)) {
 			//Fetch all table 
 			$tables['tables'][$q] = array("table_name" => $row[0]);
 				//If you want to pull more details for the table then set as true
 				if($deep){
 					//This is to fetch fields
-					$query_level = mysql_query("SHOW columns FROM ".$row[0]);
+					$query_level = mysqli_query($dbc,"SHOW columns FROM ".$row[0]);
 					if($query_level){
 						$fields = array();
-						while($fields = mysql_fetch_array($query_level)){
+						while($fields = mysqli_fetch_array($query_level)){
 							$tables['tables'][$q]['table_fields'][$v] = $fields;
 							$v++;
 						}
@@ -45,13 +43,13 @@ function get_tables(){
 }
 
 function get_table_structure($tablename){
-	db_connect();
+	$dbc = db_connect();
 	$tables = array();
-		$query_level = mysql_query("SHOW columns FROM ".$tablename);
+		$query_level = mysqli_query($dbc, "SHOW columns FROM ".$tablename);
 				if($query_level){
 						$v = 0;
 						$fields = array();
-						while($fields = mysql_fetch_array($query_level)){
+						while($fields = mysqli_fetch_array($query_level)){
 							$tables[$v] = $fields;
 							$v++;
 						}
@@ -60,15 +58,17 @@ function get_table_structure($tablename){
 }
 
 function update_simple($table,$data,$where) {
-		 db_connect();
+		 $dbc = db_connect();
 		 $query = "UPDATE $table SET $data $where"; 
-		 $result = mysql_query($query)or die("query failed ".mysql_error());
-		//$result = db_results($result);
-		return mysql_affected_rows();
+		 $result = mysqli_query($dbc,$query)or die("query failed ".mysqli_error($dbc));
+			//$result = db_results($result);
+			//var_dump($query);
+			//exit();
+		return mysqli_affected_rows($dbc);
       }
  function db_results($result){
 		$res_array = array();
-		for($count=0;$row = mysql_fetch_array($result);$count++)
+		for($count=0;$row = mysqli_fetch_array($result);$count++)
 
 		{
           	$res_array[$count] = $row;
